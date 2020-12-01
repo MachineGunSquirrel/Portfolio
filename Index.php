ad007a9d3873e825e036295dd6a4f0bebb2f9b0c
Peter Steiner <psteiner@training.local>
2020-12-01 15:04:07 -0500

    Home page

--- Index.php
+++ Index.php
@@ -2,8 +2,21 @@
   include './View/header.php';
   include './View/navbar.php';
 ?>
-<span style="font-size:30px;cursor:pointer;margin-top:2%;margin-left:2%" onclick="openNav()">&#9776;</span>
 
+<!-- Bio div -->
+<div class='bio-grid'>
+  <span id='ham' style="font-size:30px;cursor:pointer;margin-top:5px;margin-left:15px" onclick="openNav()">&#9776;</span>
+
+  <div class = 'bio'>
+    <div class='bio-img'>
+      <img src="./View/Public/Images/Profile.png" alt="">
+    </div>
+    <h2>I'm Peter, a Web Developer.</h2>
+    <h1>I work with HTML, CSS, PHP, JavaScript, and Ajax to design websites and user interfaces.
+    </h1>
+  </div>
+
+</div>
 
 <!-- Close page-grid div -->
   </div> 
--- View/navbar.php
+++ View/navbar.php
@@ -2,32 +2,36 @@
 
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
+  <a href="#">Home</a>
+  <a href="#">Projects</a>
   <a href="#">Contact</a>
+
+  <!-- Navbar Animation Javascript -->
+    <script>
+      function openNav() {
+        document.getElementById("mySidenav").style.width = "200px";
+        document.getElementById("ham").style.opacity = "0";
+      }
+
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("ham").style.opacity = "1";
+      }
+    </script>
+
+  <!-- Social Media Links -->
+    <div class='social'>
+      <!-- Twitter Follow Button -->
+        <a href="https://twitter.com/PytSkee?ref_src=twsrc%5Etfw" 
+          class="twitter-follow-button" 
+          data-size="large" data-show-screen-name="false" data-dnt="true" data-show-count="false">
+            Follow @PytSkee</a><br>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      <!-- Twitter Follow Button -->
+      <!-- Linkedin Follow Button -->
+        <script src="https://platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+        <script type="IN/FollowCompany" data-id="1337" data-counter="bottom"></script>
+      <!-- Linkedin Follow Button -->
+    </div>
+
 </div>
-
-<!-- Navbar Animation Javascript -->
-  <script>
-    function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-    }
-
-    function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-    }
-  </script>
-
-<!-- Social Media Links -->
-  <!-- Twitter Follow Button -->
-    <!-- <a href="https://twitter.com/PytSkee?ref_src=twsrc%5Etfw" 
-      class="twitter-follow-button" 
-      data-size="large" data-show-screen-name="false" data-dnt="true" data-show-count="false">
-        Follow @PytSkee</a>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
-  <!-- Twitter Follow Button -->
-  <!-- Linkedin Follow Button -->
-    <!-- <script src="https://platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
-    <script type="IN/FollowCompany" data-id="1337" data-counter="bottom"></script> -->
-  <!-- Linkedin Follow Button -->
